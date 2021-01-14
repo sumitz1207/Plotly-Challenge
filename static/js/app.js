2d11b1df4583c138af6c3ad5179695f0053d5965
Sumit Patel <sumitz1207@gmail.com>
2021-01-14 15:12:13 -0500

    added bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -13,10 +13,60 @@ function getSampleData(subject) {
         dataSelect.append("h6").text(`${k.toUpperCase()}: ${v}`);
       });
     });
-  }
+}
+
+function getPlot(subject) {
+    d3.json("samples.json").then((d) => {
+     //filter samples.json data and take IDs, labels, and values
+      var filtered = d.samples.filter(filteredItem => filteredItem.id == subject);  
+      var subjectIDs = filtered[0].otu_ids;
+      var subjectLabels = filtered[0].otu_labels;
+      var subjectValues = filtered[0].sample_values;
+  
+      //assign layout for the bubble chart
+      var layout1 = {
+        title: "Bacteria ID by Sample",
+        xaxis: { title: "OTU ID" },
+      };
+      //assign data for the bubble chart
+      var trace1 =         {
+        x: subjectIDs,
+        y: subjectValues,
+        mode: "markers",
+        text: subjectLabels,
+        marker: {
+          size: subjectValues,
+          color: subjectIDs,
+        }
+      }
+      var data1 = [trace1];
+      //use plotly to organize new Plot for bubble chart
+      Plotly.newPlot("bubble", data1, layout1);
+      //Bar chart
+      //assign trace for the bar chart
+      var trace2 =          {
+        y: subjectIDs.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+        x: subjectValues.slice(0, 10).reverse(),
+        text: subjectLabels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+      //trace into data for bar chart
+      var data2 = [trace2];
+      //assign layout for the bar chart
+      var layout2 = {
+        title: "Top 10 Bacteria IDs",
+        margin: { t: 30, l: 150 }
+      };
+      //use plotly to make the bar chart
+      Plotly.newPlot("bar", data2, layout2);
+
+
+    });
+}
 
 //initialize function for plotly app
-  function init() {
+function init() {
     //refer to drop down data selector
     var dropSelect = d3.select("#selDataset");
   
@@ -28,10 +78,16 @@ function getSampleData(subject) {
       names.forEach((listItem) => {dropSelect.append("option").text(listItem).property("value", listItem);
       });
       //take from the list and call the functions to build graphs and data
-      getPlots(names[0]);
+      getPlot(names[0]);
       getSampleData(names[0]);
     });
-  }
+}
+
+function optionChanged(subject) {
+    // Fetch new data each time a new sample is selected
+    getPlot(subject);
+    getSampleData(subject);
+}
 
   //call initialize for app
-  init();
+init();
